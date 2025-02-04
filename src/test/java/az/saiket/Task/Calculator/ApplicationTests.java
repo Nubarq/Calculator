be5bf5f29bea0db92d261d9a1a89cdbb2bed9be8
Nübar Qasımova <149222701+Nubarq@users.noreply.github.com>
2025-02-04 21:39:37 +0400

    db conf

---
+++ src/test/java/az/saiket/Task/Calculator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.saiket.Task.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
